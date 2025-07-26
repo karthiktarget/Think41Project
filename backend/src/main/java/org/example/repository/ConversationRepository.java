5a177fa724585226a80bf9456e06db878e45caf4
karthiktarget <karthiktarget2004@gmail.com>
2025-07-26 14:06:25 +0530

    made some changes in the file structure

--- backend/src/main/java/org/example/repository/ConversationRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.repository;
-
-import org.example.model.Conversation;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.UUID;
-
-public interface ConversationRepository extends JpaRepository<Conversation, UUID> {}
